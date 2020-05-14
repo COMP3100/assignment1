d39e86bbcc1e862d923be672f670eff11df2c32a
siobhan-maree <siobhan_maree@hotmail.com>
2020-05-14 19:18:33 +1000

    Add files via upload

---
+++ BestFit/Client (1).java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+	
+public class Client {
+	//Declare global variables
+	static Socket s = null;
+	static String fromServer = "";
+	static String[] jobDetails = null;
+	static String schedulingDecision = "";
+	static String[] firstAvailServer = null;
+	static List<Server> allServers = null;
+	
+	public static void main(String[] args) {
+		try {
+			//Create socket connection
+			s = new Socket("127.0.0.1", 50000);
+
+			//Exchanging with the Server
+			writeToServer(s, "HELO");
+			fromServer = readFromServer(s);
+
+			writeToServer(s, "AUTH " + System.getProperty("user.name"));
+			fromServer = readFromServer(s);
+			
+			//create a list of servers from system.xml
+			allServers = ReadXMLFile.parseSystemXML("system.xml");
+			
+			//Continue communication with Server
+			writeToServer(s, "REDY");
+			fromServer = readFromServer(s);			
+
+			//loop through all jobs to be scheduled
+			while (!fromServer.equals("NONE")) {
+				jobDetails = fromServer.split(" "); 
+				
+				if (args.length > 0) {
+					if (args[1].equals("ff")) {
+						firstFit();
+					} else if (args[1].contentEquals("bf")) {
+						//best-fit
+					}
+				} else {
+					allToLargest();
+				}
+				
+				writeToServer(s, schedulingDecision);
+				fromServer = readFromServer(s);
+
+				writeToServer(s, "REDY");
+				fromServer = readFromServer(s);
+			}
+
+			writeToServer(s, "QUIT");
+			fromServer = readFromServer(s);
+			s.close();
+
+		} catch (IOException e) {
+			System.out.println("IO Exception in main " + e);
+		}
+
+	}
+	
+	public static void writeToServer(Socket s, String message) {
+		try {
+			PrintWriter out = new PrintWriter(s.getOutputStream());
+			out.println(message);
+			out.flush();
+		} catch (IOException e) {
+			System.out.println("IOException in writeToServer " + e);
+		}
+	}
+	
+	public static String readFromServer(Socket s) {
+		String input = "";
+		try {
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bufIn = new BufferedReader(in);
+			input = bufIn.readLine();
+		} catch (IOException e) {
+			System.out.println("IOException in readFromServer " + e);
+		}
+		return input;
+	}
+	
+	public static void allToLargest() {
+		int largest = 0; 
+		for (int i = 1; i < allServers.size(); i++) {
+			if (allServers.get(largest).coreCount < allServers.get(i).coreCount) {
+				largest = i;
+			}
+		}
+		schedulingDecision = "SCHD " + jobDetails[2] + " " + allServers.get(largest).type + " " + "0";
+	}
+	
+	public static void firstFit() {
+		String[] serverDetails = null;
+		
+		writeToServer(s, "RESC Avail " + jobDetails[4] + " " + jobDetails[5] + " " + jobDetails[6]);
+		fromServer = readFromServer(s);
+
+		//loop through the server details
+		while (!fromServer.equals(".")) {
+			if (fromServer.contentEquals("DATA")) {
+				writeToServer(s, "OK");
+				fromServer = readFromServer(s);
+				if (fromServer.contentEquals(".")) {
+					allToLargest();
+					break;
+				} else {
+				serverDetails = fromServer.split(" ");
+				schedulingDecision = "SCHD " + jobDetails[2] + " " + serverDetails[0] + " " + serverDetails[1];
+				}
+			}
+			writeToServer(s, "OK");
+			fromServer = readFromServer(s);
+		}
+	}
+	
+	public static void bestFit() {
+		String[] serverDetails = null;
+		
+		writeToServer(s, "RESC All");
+		//readfromServer
+		//while fromServer != "."
+			//if fromServer == "DATA"
+				//writeToServer "OK"
+				//readfromServer
+				//if fromServer == "."
+					//run first fit
+					//break;
+			//fromServer.split(" ")
+			//if this server has sufficent resources to run job
+				//calculate fitness value (coreCount - jobDetails[4])
+				//calculate available time (jobDetails[3])
+				//if this server has a better fitness value then current best OR it has an equal fitness value AND a better available time
+					//set this server to current best
+					//set minAvailTime to this available time 
+			//writeToServer "OK"
+		//set scheduling decision to current best
+		
+		int fitnessValue = 1000;
+		int tempFV = Integer.parseInt(jobDetails[4]);
+	}
+	
+}
